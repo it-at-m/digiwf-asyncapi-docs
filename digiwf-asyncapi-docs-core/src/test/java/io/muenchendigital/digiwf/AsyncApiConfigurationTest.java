150485ea50c374fbe0a8225ee44b758881edf7cb
Lukas Mösle <ext.dl.moesle@muenchen.de>
2022-05-17 11:23:56 +0200

    refactoring

--- digiwf-asyncapi-docs-core/src/test/java/io/muenchendigital/digiwf/AsyncApiConfigurationTest.java
+++ digiwf-asyncapi-docs-core/src/test/java/io/muenchendigital/digiwf/AsyncApiConfigurationTest.java
@@ -1,36 +1,34 @@
 package io.muenchendigital.digiwf;
 
 import io.github.stavshamir.springwolf.configuration.AsyncApiDocket;
-import io.github.stavshamir.springwolf.schemas.DefaultSchemasService;
-import io.github.stavshamir.springwolf.schemas.SchemasService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class AsyncApiConfigurationTest {
 
-    private final SchemasService schemasService = new DefaultSchemasService();
-    private final String basePackage = "io.muenchendigital.digiwf";
-    private final String binder = "kafka";
-    private final String broker = "localhost:9092";
-    private final String version = "1.0.0";
-    private final String title= "Kafka Test";
-
     @Test
     void asyncApiDocket() {
+
+        final String basePackage = "io.muenchendigital.digiwf";
+        final String binder = "kafka";
+        final String broker = "localhost:9092";
+        final String version = "1.0.0";
+        final String title = "Kafka Test";
+
         final AsyncApiConfiguration asyncApiConfiguration = new AsyncApiConfiguration(
-                this.binder,
-                this.broker,
-                this.basePackage,
-                this.version,
-                this.title
+                binder,
+                broker,
+                basePackage,
+                version,
+                title
         );
         final AsyncApiDocket docket = asyncApiConfiguration.asyncApiDocket();
 
-        Assertions.assertEquals(this.basePackage, docket.getBasePackage());
-        Assertions.assertEquals(this.broker, docket.getServers().get("kafka").getUrl());
-        Assertions.assertEquals(this.binder, docket.getServers().get("kafka").getProtocol());
-        Assertions.assertEquals(this.version, docket.getInfo().getVersion());
-        Assertions.assertEquals(this.title, docket.getInfo().getTitle());
+        Assertions.assertEquals(basePackage, docket.getBasePackage());
+        Assertions.assertEquals(broker, docket.getServers().get("kafka").getUrl());
+        Assertions.assertEquals(binder, docket.getServers().get("kafka").getProtocol());
+        Assertions.assertEquals(version, docket.getInfo().getVersion());
+        Assertions.assertEquals(title, docket.getInfo().getTitle());
     }
 
 }
